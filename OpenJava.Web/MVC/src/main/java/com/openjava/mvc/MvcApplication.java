35405c6fefa457c7f6496774f3befd59affc6fe7
TerryDan <dantao332@msn.cn>
2019-11-05 14:02:48 +0800

    add mybatis demo

--- OpenJava.Web/MVC/src/main/java/com/openjava/mvc/MvcApplication.java
+++ OpenJava.Web/MVC/src/main/java/com/openjava/mvc/MvcApplication.java
@@ -1,9 +1,11 @@
 package com.openjava.mvc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.openjava.mvc.mapper")
 public class MvcApplication {
 
 	public static void main(String[] args) {
--- OpenJava.Web/MVC/src/main/java/com/openjava/mvc/controller/HomeController.java
+++ OpenJava.Web/MVC/src/main/java/com/openjava/mvc/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.openjava.mvc.controller;
 
+import com.openjava.mvc.model.UserModel;
+import com.openjava.mvc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +12,21 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    private final UserService service;
+
+    @Autowired
+    public HomeController(UserService service) {
+        this.service = service;
+    }
+
     @RequestMapping("/")
     public String Index(Map<String, Object> paramMap) {
 
         /** 默认Map的内容会放大请求域中，页面可以直接取值*/
-        paramMap.put("name", "zhangSan");
-        paramMap.put("age", 28);
+        UserModel user=this.service.select(1);
+        paramMap.put("username", user.getUserName());
+        paramMap.put("realname", user.getRealName());
 
-        /** 会自动跳转到默认的 classpath:/templates/index.html 页面*/
         return "index";
     }
 
